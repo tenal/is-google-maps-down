c403b881da7dc59dc0c4413d743e0ce0c0592dd0
Jeremy Ahn <me@jeremyahn.com>
2017-09-17 12:42:08 -0400

    asyncronous bug fixed

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	var works = true;
-	http.get('http://maps.google.com/', function() {
+	http.get('http://maps.google.com', function() {
 		// Google Maps works -- this is the most likely (by far) scenario
 		//works = true;
+		res.render('index', { works: works });
 	}).on('error', function() {
 		// Google Maps doesn't work!
 		works = false;
+		res.render('index', { works: works });
 	});
-	res.render('index', { works: works });
 });
 
 module.exports = router;
